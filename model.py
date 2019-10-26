c7ecf687b14753e64a42b6bea16ef8e3499b8343
Anuj Menta <anujmenta@gmail.com>
2019-10-26 16:15:34 -0400

    parameter tuning, differntial lr debug4

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
         self.batchnorm1 = nn.BatchNorm2d(100)
-        self.conv2 = nn.Conv2d(100, 200, kernel_size=3)
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=5)
         self.batchnorm2 = nn.BatchNorm2d(200)
         self.conv3 = nn.Conv2d(200, 250, kernel_size=3)
         self.batchnorm3 = nn.BatchNorm2d(250)
