cf0ee2c660d93bc1aefed036f609a7c266236046
Anuj Menta <anujmenta@gmail.com>
2019-10-26 16:13:20 -0400

    parameter tuning, differntial lr debug2

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ if use_gpu:
 acc_tracker = []
 
 #optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-lastlayers = list(model.fc1.parameters() + model.fc2.parameters())
-middlelayers = list(model.conv2.parameters() + model.batchnorm2.parameters() + model.conv3.parameters() + model.batchnorm3.parameters)
+lastlayers = list(model.fc1.parameters()) + list(model.fc2.parameters())
+middlelayers = list(model.conv2.parameters())+list(model.batchnorm2.parameters())+list(model.conv3.parameters())+list(model.batchnorm3.parameters)
 optimizer = optim.Adam([
         {"params" : lastlayers, "lr":1e-3},
         {"params" : middlelayers, "lr":4e-2}
