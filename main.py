39e808448f1dce1a8e61d62c6651bf2beb5d8713
Anuj Menta <anujmenta@gmail.com>
2019-10-26 18:48:27 -0400

    roll back to sgd

--- main.py
+++ main.py
@@ -86,10 +86,10 @@ if use_gpu:
 
 acc_tracker = []
 
-#optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-lastlayers = list(model.fc1.parameters()) + list(model.fc2.parameters())
-middlelayers = list(model.conv2.parameters())+list(model.batchnorm2.parameters())+list(model.conv3.parameters())+list(model.batchnorm3.parameters())
-optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+# lastlayers = list(model.fc1.parameters()) + list(model.fc2.parameters())
+# middlelayers = list(model.conv2.parameters())+list(model.batchnorm2.parameters())+list(model.conv3.parameters())+list(model.batchnorm3.parameters())
+#optimizer = optim.AdamW(model.parameters(), lr=args.lr)
 scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.01, cycle_momentum=False)
 
 def train(epoch):
