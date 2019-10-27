95e9a8ff91e796d3c79733a9e4ab427337feef68
Anuj Menta <anujmenta@gmail.com>
2019-10-27 14:45:17 -0400

    trying to print lr

--- main.py
+++ main.py
@@ -135,6 +135,8 @@ def validation():
 for epoch in range(1, args.epochs + 1):
     train(epoch)
     validation()
+    for param_group in optimizer.param_groups:
+        print(param_group[‘lr’])
     model_file = 'model_' + str(epoch) + '.pth'
     torch.save(model.state_dict(), model_file)
     print('\nSaved model to ' + model_file + '. You can run `python evaluate.py --model ' + model_file + '` to generate the Kaggle formatted csv file')
