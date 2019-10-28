d32c13cdf3459d6c46da07eddcf656b0cf6fe02b
Anuj Menta <anujmenta@gmail.com>
2019-10-28 18:50:32 -0400

    added spatial transform

--- model.py
+++ model.py
@@ -17,8 +17,39 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(250, 100)
         self.fc2 = nn.Linear(100, nclasses)
 
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 3 * 3, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
     def forward(self, x):
         bt_size = x.size(0)
+
+        x = self.stn(x)
         x = self.batchnorm1(F.max_pool2d(F.leaky_relu(self.conv1(x)),2))
         x = self.dropout(x)
         x = self.batchnorm2(F.max_pool2d(F.leaky_relu(self.conv2(x)),2))
