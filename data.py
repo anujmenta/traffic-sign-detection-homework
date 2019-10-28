962de392e8387e00926db0dd1f7e28e302fc36a6
Anuj Menta <anujmenta@gmail.com>
2019-10-28 12:45:32 -0400

    best model so far. changing prob 1 to 0.7

--- data.py
+++ data.py
@@ -86,21 +86,21 @@ data_transform_centercrop = transforms.Compose([
 
 data_transform_hrflip = transforms.Compose([
     transforms.Resize((32, 32)),
-    transforms.RandomHorizontalFlip(1),
+    transforms.RandomHorizontalFlip(0.7),
     transforms.ToTensor(),
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
 
 data_transform_vrflip = transforms.Compose([
     transforms.Resize((32, 32)),
-    transforms.RandomVerticalFlip(1),
+    transforms.RandomVerticalFlip(0.7),
     transforms.ToTensor(),
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
 
 data_transform_bothflip = transforms.Compose([
     transforms.Resize((32, 32)),
-    transforms.RandomHorizontalFlip(1),
+    transforms.RandomHorizontalFlip(0.7),
     transforms.RandomVerticalFlip(1),
     transforms.ToTensor(),
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
