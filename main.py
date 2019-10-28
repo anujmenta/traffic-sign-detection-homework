5e165874bad55f9571b6b4834d523c78a67ad603
Anuj Menta <anujmenta@gmail.com>
2019-10-28 11:49:52 -0400

    remove weight decay

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ acc_tracker = []
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=0.01)
 # lastlayers = list(model.fc1.parameters()) + list(model.fc2.parameters())
 # middlelayers = list(model.conv2.parameters())+list(model.batchnorm2.parameters())+list(model.conv3.parameters())+list(model.batchnorm3.parameters())
-optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.01)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5)
 scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.01, cycle_momentum=False)
 def train(epoch):
